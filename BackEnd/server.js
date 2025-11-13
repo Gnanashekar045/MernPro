4df1cb11a6fa63b023d130ef3c681ad742acb2ee
Gnanashekar K <gnanashekar045@gmail.com>
2025-11-13 16:26:26 +0530

    project complition stage

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,7 +1,20 @@
 import express from 'express'
+import dotenv from 'dotenv'
 
 const app = express()
 
-app.listen(5000, ()=> {
-    console.log("Server started at http//localhost:5000 ss")
+dotenv.config()
+
+app.get("/", (req, res)=> {
+    res.send("Server is ready")
 })
+
+console.log(process.env.MONG_URI)
+
+app.listen(5000, ()=> {
+    console.log("Server started at http//localhost:5000")
+})
+
+//keNxKAyXwFTGmxmO
+
+//mongodb+srv://shekark509_db_user:keNxKAyXwFTGmxmO@tesk1.6cuqbpc.mongodb.net/?appName=tesk1
